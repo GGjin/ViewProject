10c65de93b9154f28a398619887af12f25ecf260
GG <gjy@linkcity.cc>
2017-11-06 14:00:24 +0800

    Initial commit

---
+++ app/src/main/java/com/gg/viewproject/MyTextView.kt
@@ -0,0 +1,91 @@
+package com.gg.viewproject
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.util.Log
+import android.util.TypedValue
+import android.widget.LinearLayout
+
+/**
+ *  Creator : GG
+ *  Time    : 2017/11/3
+ *  Mail    : gg.jin.yu@gmail.com
+ *  Explain :
+ */
+class MyTextView : LinearLayout {
+
+    private var mText: String
+    private var mTextSize = 15
+    private var mTextColor = Color.BLACK
+    private var mPaint: Paint
+
+    constructor(context: Context?) : this(context, null)
+    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+        val array = context?.obtainStyledAttributes(attrs, R.styleable.MyTextView)
+        mText = array?.getString(R.styleable.MyTextView_text)!!
+        mTextSize = array.getDimensionPixelSize(R.styleable.MyTextView_textSize, sp2Px(mTextSize))
+        mTextColor = array.getColor(R.styleable.MyTextView_textColor, mTextColor)
+        array.recycle()
+
+        mPaint = Paint()
+
+        mPaint.isAntiAlias = true
+        mPaint.textSize = mTextSize.toFloat()
+        mPaint.color = mTextColor
+
+//        //设置默认背景
+//        setBackgroundColor(Color.TRANSPARENT)
+
+        //重新设置Flags
+        setWillNotDraw(false)
+    }
+
+    private fun sp2Px(sp: Int): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp.toFloat(), resources.displayMetrics).toInt()
+    }
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+
+        var width = MeasureSpec.getSize(widthMeasureSpec)
+        var bound = Rect()
+        mPaint.getTextBounds(mText, 0, mText.length, bound)
+        if (widthMode == MeasureSpec.AT_MOST) {
+
+            width = bound.width() + paddingLeft + paddingRight
+        }
+
+        var height = MeasureSpec.getSize(heightMeasureSpec)
+
+        if (heightMode == MeasureSpec.AT_MOST) {
+            height = bound.height()+paddingTop+paddingBottom
+        }
+        setMeasuredDimension(width, height)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        var fontMetricsInt = mPaint.fontMetricsInt
+        Log.d("top", "------   " + fontMetricsInt.top)
+        Log.d("bottom", "------   " + fontMetricsInt.bottom)
+        Log.d("ascent", "------   " + fontMetricsInt.ascent)
+        Log.d("descent", "------   " + fontMetricsInt.descent)
+        Log.d("leading", "------   " + fontMetricsInt.leading)
+
+        var baseLine = height / 2 + (fontMetricsInt.bottom - fontMetricsInt.top) / 2 - fontMetricsInt.bottom
+
+        canvas?.drawText(mText, paddingLeft.toFloat(), baseLine.toFloat(), mPaint)
+    }
+
+}
+
